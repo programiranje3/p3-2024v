2ba5de095bfae19446896aeee26b9e086f2934c6
jeljov <jeljov@gmail.com>
2024-10-04 23:00:50 +0200

    Tasks for Lab1 added

---
+++ lab1/lab1_start.py
@@ -0,0 +1,88 @@
+#%%
+# Zadatak 1
+
+
+
+#%%
+# Test the function
+# odd_or_even()
+
+
+#%%
+# Zadatak 2
+
+
+
+
+
+#%%
+# Test the function
+# print(factorial(5))
+
+
+#%%
+# Zadatak 3
+
+
+
+
+
+#%%
+# Test the function with...
+# ... a sequence of numbers:
+# numbers  = [31, 72, 13, 41, 5, 16, 87, 98, 9]
+# print(f"3rd lowest among numbers: {numbers}:")
+# print(nth_lowest(numbers,3))
+
+# ... a sequence of letters:
+# letters = ['f', 'r', 't', 'a', 'b', 'y', 'j', 'd', 'c']
+# print(f"6th lowest among letters: {letters}:")
+# print(nth_lowest(letters, 6))
+
+# ... a string:
+# s = "today"
+# print(f"2nd lowest in string: '{s}':")
+# print(nth_lowest(s, 2))
+
+
+#%%
+# Zadatak 4
+
+
+
+
+
+
+#%%
+# Test the function
+# print(list_stats([3.4, 5.6, -4.2, -5.6, 9, 1.2, 11.3, -23.45, -81]))
+
+
+#%%
+# Zadatak 5
+
+
+
+
+
+
+#%%
+# Test the function
+# list_operations([1, 1, 2, 3, 5, 8, 13, 5, 21, 34, 55, 89], 20)
+
+
+
+#%%
+# Zadatak 6
+
+
+
+
+
+
+
+
+#%%
+# Test the function
+# guessing_game()
+
