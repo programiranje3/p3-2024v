865d85ac1e378692ea127b8eeac71a7cc9f6a652
jeljov <jeljov@gmail.com>
2024-10-12 17:38:28 +0200

    Lab2 tasks added

---
+++ lab2/lab2_start.py
@@ -0,0 +1,94 @@
+#%%
+# Zadatak 1
+
+
+
+
+#%%
+# Test the function
+# list1 = ["M", "na", "i", "Ke"]
+# list2 = ["y", "me", "s", "lly"]
+# print(concat_index_wise(list1, list2))
+
+#%%
+# Zadatak 2
+
+
+
+
+#%%
+# Test the function
+# s1 = "Tokyo's 2024 population is now estimated at 37,115,035."
+# s2 = "Tokyo is one of the most populated cities."
+# print(digits_in_string(s1))
+# print(digits_in_string(s2))
+
+#%%
+# Zadatak 3
+
+
+
+
+#%%
+# Test the function
+# s1 = "potop"
+# print(f"{s1}: {palindrom(s1)}")
+# s2 = "Sir ima miris"
+# print(f"{s2}: {palindrom(s2)}")
+# s3 = "ananas"
+# print(f"{s3}: {palindrom(s3)}")
+
+#%%
+# Zadatak 4
+
+
+
+
+#%%
+# Test the function
+# passwords_to_check = "ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR"
+# print(f"Passwords to check: {passwords_to_check}")
+# passwords_check(passwords_to_check)
+
+
+#%%
+# Zadatak 5
+
+
+
+
+#%%
+# Test the function
+# server_state_log = '''
+#     Server abc01 is up
+#     Server abc02 is down
+#     Server abc03 is down
+#     Server xyz01 is up
+#     Server xyz02 is up
+#     Server abc02 is up
+#     Server abc01 is down
+#     '''
+# server_status(server_state_log)
+
+
+#%%
+# Zadatak 6
+
+
+
+
+#%%
+# Test the function
+# print(anagram('ortoped', 'torpedo'))
+# print(anagram('ortopedi', 'torpedo'))
+# print(anagram('On sa tla Like', 'Nikola Tesla'))
+
+#%%
+# Zadatak 7
+
+
+
+
+#%%
+# Test the function
+# all_even_digits()
