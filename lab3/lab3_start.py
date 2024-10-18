fbc5df0693631ab5d1527e22608e4ebb4e4dbc04
jeljov <jeljov@gmail.com>
2024-10-18 18:18:46 +0200

    Lab3 tasks added

---
+++ lab3/lab3_start.py
@@ -0,0 +1,100 @@
+"""
+Vezbe, dvocas 3
+"""
+
+
+#%%
+# Zadatak 1
+
+
+
+
+#%%
+# create_print_numeric_dict(7)
+
+#%%
+# Zadatak 2
+
+
+
+
+#%%
+# dishes = ["pizza", "sauerkraut", "paella", "hamburger"]
+# countries = ["Italy", "Germany", "Spain", "USA", "Serbia"]
+# lists_to_dict(countries, dishes)
+
+#%%
+# Zadatak 3
+
+
+
+
+#%%
+# print("string_stats('Today is October 24, 2023!'):")
+# print(string_stats("Today is October 24, 2023!"))
+
+#%%
+# Zadatak 4
+
+
+
+
+#%%
+# response by GPT-3 to the question of why it has so entranced the tech community
+# source: https://www.wired.com/story/ai-text-generator-gpt-3-learning-language-fitfully/
+# gpt3_response = ("""
+#     I spoke with a very special person whose name is not relevant at this time,
+#     and what they told me was that my framework was perfect. If I remember correctly,
+#     they said it was like releasing a tiger into the world.
+# """)
+# token_frequency(gpt3_response)
+
+#%%
+# Zadatak 5
+
+
+
+
+#%%
+# print("Passwords to check: ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456wr")
+# validation_dict = password_check("ABd1234@1, a F1#,2w3E*,2We334#5, t_456wr")
+# print("Validation results:")
+# for password, result in validation_dict.items():
+#     print(f"- {password}: {', '.join(result)}")
+
+#%%
+# Zadatak 6
+
+
+
+
+#%%
+# team = [{'name': 'Bob', 'age': 18, 'score': 50.0},
+#         {'name': 'Tim', 'age': 17, 'score': 84.0},
+#         {'name': 'Jim', 'age': 22, 'score': 94.0},
+#         {'name': 'Joe', 'age': 19, 'score': 85.5}]
+# team_stats(team)
+
+#%%
+# Zadatak 7
+
+
+
+
+#%%
+# l = [('V', 1), ('VI', 1), ('V', 2), ('VI', 2), ('VI', 3), ('VII', 1)]
+# class_stats(l)
+
+#%%
+# Zadatak 8
+
+
+
+
+#%%
+
+
+
+
+
+
