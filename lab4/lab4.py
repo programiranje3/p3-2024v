173b5235db2445bab0007ae9628e0f6df1580eec
jeljov <jeljov@gmail.com>
2024-11-05 22:11:58 +0100

    Lab4 code completed; Lab5, class Putnik added

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -93,12 +93,28 @@ print(process_product_orders(orders, discount=10))
 
 #%%
 # Zadatak 4
+import functools
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
 
+        # Nešto uradite pre
+        from time import perf_counter
+        start_time = perf_counter()
+
+        value = func(*args, **kwargs)
+
+        # Nešto uradite posle
+        working_time = perf_counter() - start_time
+        print(f"Function {func.__name__} completed its task in {working_time:.4f} seconds")
+
+        return value
+    return wrapper_timer
 
 
 #%%
 # Zadatak 4.1
-
+@timer
 def compute_sum_loop(n):
     sum_of_sums = 0
     for x in range(n+1):
@@ -107,11 +123,11 @@ def compute_sum_loop(n):
             sum_of_sums += i
     return sum_of_sums
 
-
+@timer
 def compute_sum_lc(n):
     return sum([sum(range(x+1)) for x in range(n+1)])
 
-
+@timer
 def compute_sum_mr(n):
     from functools import reduce
     mapping = map(lambda x: sum(range(x+1)), range(n+1))
@@ -119,36 +135,79 @@ def compute_sum_mr(n):
 
 
 #%%
-# print(compute_sum_loop(10000))
-# print()
-# print(compute_sum_lc(10000))
-# print()
-# print(compute_sum_mr(10000))
+print(compute_sum_loop(10000))
+print()
+print(compute_sum_lc(10000))
+print()
+print(compute_sum_mr(10000))
 
 #%%
 # Zadatak 4.2
 
+@timer
+def mean_median_diff(n, k, iterations=10):
+    from random import randint, seed
+    from statistics import mean, median
 
+    seed(1)
+    diffs = []
+    for _ in range(iterations):
+        random_numbers = []
+        for _ in range(n):
+            random_numbers.append(randint(1, k))
+        diffs.append(mean(random_numbers) - median(random_numbers))
+
+    for i, diff in enumerate(diffs):
+        print(f"Iteration {i+1}: {diff:.4f}")
+
+    print()
+    print(f"Average difference across all iterations: {mean(diffs):.4f}")
 
 
 #%%
-# mean_median_diff(100, 250, iterations=20)
+mean_median_diff(100, 250, iterations=20)
 
 
 #%%
 #Zadatak 5
 
+def standardiser(func):
+    @functools.wraps(func)
+    def wrapper_standardiser(*args, **kwargs):
 
+        # Nešto uradite pre
+        if all([isinstance(arg, (int, float)) for arg in args]):
+            from statistics import mean, stdev
+            m = mean(args)
+            sd = stdev(args)
+            args = [(arg - m)/sd for arg in args]
+        else:
+            print("Not all positional arguments are numbers. Keeping the arguments as given")
+
+        func_str = f"Calling function {func.__name__} with positional arguments: "
+        func_str += ", ".join([f'{arg:.4f}' for arg in args])
+        if kwargs:
+            func_str += "\nand keyword arguments: " + ", ".join(f"{key}={val}" for key, val in kwargs.items())
+        print(func_str)
+
+        value = func(*args, **kwargs)
+
+        # Nešto uradite posle
+        value = round(value, 4)
+
+        return value
+    return wrapper_standardiser
 
 
 
 #%%
 # Zadatak 5.1
 
+@standardiser
 def sum_of_sums(*numbers, n=10):
     return sum([sum([x**i for i in range(n+1)]) for x in numbers])
 
 
 #%%
-# print(sum_of_sums(1,3,5,7,9,11,13, n=7))
+print(sum_of_sums(1,3,5,7,9,11,13, n=7))
 
